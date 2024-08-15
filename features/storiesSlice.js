1151107d86186f63997be74d6badba9ae9a4debf
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-08-15 13:12:21 +0100

    first version

--- features/storiesSlice.js
+++ features/storiesSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { getData, storeData } from "../storageUtilis";
 
 export const fetchStory = createAsyncThunk('story/fetchStory', async () => {
     const response = await axios.get('https://shortstories-api.onrender.com/stories');
@@ -13,6 +14,8 @@ const storySlice = createSlice({
         error: null,
         status: 'idle',
         selectedStory: null,
+        readStories: [],
+        bookmarks: [],
     },
     reducers: {
         selectStory: (state, action) => {
@@ -20,7 +23,25 @@ const storySlice = createSlice({
         },
         clearSelectedStory: (state) => {
             state.selectedStory = null;
-        }
+        },
+        markAsRead: (state, action) => {
+            if (!state.readStories.includes(action.payload)) {
+                state.readStories.push(action.payload);
+                storeData('readStory', state.readStories)
+            }
+        },
+        toggleBookmark: (state, action) => {
+            if(state.bookmarks.includes(action.payload)) {
+                state.bookmarks = state.bookmarks.filter(bookmark => bookmark !== action.payload)
+            } else {
+                state.bookmarks.push(action.payload);
+            }
+            storeData('bookmarks', state.bookmarks);
+        },
+        setInitialData: (state, action) => {
+            state.readStories = action.payload.readStories || [];
+            state.bookmarks = action.payload.bookmarks || [];
+        },
     },
     extraReducers: (builders) => {
         builders.addCase(fetchStory.pending, (state) => {
@@ -35,5 +56,5 @@ const storySlice = createSlice({
     }
 })
 
-export const { selectStory, clearSelectedStory } = storySlice.actions
+export const { selectStory, clearSelectedStory, markAsRead, toggleBookmark, setInitialData } = storySlice.actions
 export default storySlice.reducer;
--- screens/Authorized.js
+++ screens/Authorized.js
@@ -1,29 +1,50 @@
-import { View, Text, FlatList, ActivityIndicator, TouchableOpacity, Button, StyleSheet, Modal, ScrollView } from 'react-native'
+import { View, Text, FlatList, ActivityIndicator, TouchableOpacity, Button, StyleSheet, Modal, ScrollView, Switch } from 'react-native'
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchStory, selectStory, clearSelectedStory } from '../features/storiesSlice';
+import { fetchStory, selectStory, clearSelectedStory, markAsRead, toggleBookmark, setInitialData } from '../features/storiesSlice';
+import { getData } from '../storageUtilis';
 
 const Authorized = ({ user, handleAuthentication }) => {
-
     const stories = useSelector((state) => state.story.stories);
     const status = useSelector((state) => state.story.status);
     const error = useSelector((state) => state.story.error);
     const dispatch = useDispatch();
     const selectedStory = useSelector((state) => state.story.selectedStory);
     const [modalVisible, setModalVisible] = useState(false);
+    const [isEnabled, setIsEnabled] = useState(false);
+    const readStories = useSelector(state => state.story.readStories);
+    const bookmarks = useSelector((state) => state.story.bookmarks);
+
+
+    const toggleSwitch = () => {
+        setIsEnabled(prev => !prev)
+    }
 
     useEffect(() => {
         dispatch(fetchStory());
+
+        const loadInitialData = async () => {
+            const readStoriesData = await getData('readStory');
+            const bookmarksData = await getData('bookmarks');
+            dispatch(setInitialData({ readStories: readStoriesData, bookmarks: bookmarksData }))
+        };
+
+        loadInitialData();
     }, [dispatch]);
 
     const handleStoryDetails = (story) => {
         dispatch(selectStory(story));
+        dispatch(markAsRead(story._id))
         setModalVisible(true);
     }
 
     const handleClearStory = () => {
         dispatch(clearSelectedStory());
-        setModalVisible(false);
+        setModalVisible(!modalVisible);
+    }
+
+    const handleBookmark = (story) => {
+        dispatch(toggleBookmark(story._id))
     }
 
     if (status === 'loading') {
@@ -42,28 +63,48 @@ const Authorized = ({ user, handleAuthentication }) => {
         )
     }
 
+    const myColor = {color: isEnabled ? '#E0E0E0' : '#3A3967'}
+    const myBackground = {backgroundColor: isEnabled ? '#121212' : '#FBF7EF'}
+    const myColor2 = {color: isEnabled ? '#E0E0E0' : '#3A3967'}
+
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, myBackground]}>
             <View style={styles.top}>
-                <Text style={styles.read}>Readium</Text>
+                <Text style={[styles.read, myColor]}>Readium</Text>
                 <View style={styles.userSection}>
-                    <Text style={styles.userEmail}>{user.email}</Text>
+                    <Text style={[styles.userEmail, myColor]}>{user.email}</Text>
                     <TouchableOpacity onPress={handleAuthentication}>
                         <Text style={styles.logoutButton}>Logout</Text>
                     </TouchableOpacity>
+                    <Switch
+                    trackColor={{false: '#121212', true: '#FBF7EF'}}
+                    thumbColor={isEnabled ? '#121212' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}  />
                 </View>
             </View>
 
             <FlatList
                 data={stories}
                 keyExtractor={(item) => item._id.toString()}
-                renderItem={({ item }) => (
-                    <TouchableOpacity style={styles.storyItem} onPress={() => handleStoryDetails(item)}>
-                        <Text style={styles.storyTitle}>{item.title}</Text>
+                renderItem={({ item }) => {
+                    const isRead = readStories.includes(item._id);
+                    const isBookmarked = bookmarks.includes(item._id);
+
+                    return (
+                        <TouchableOpacity style={[styles.storyItem, {backgroundColor: isEnabled ? '#E0E0E0' : '#fff' }]} onPress={() => handleStoryDetails(item)}>
+                        <Text style={[styles.storyTitle, isRead ? styles.readText : null]}>{item.title}</Text>
                         <Text style={styles.storyAuthor}>By: {item.author}</Text>
                         <Text style={styles.storyMoral}>Moral: {item.moral}</Text>
-                    </TouchableOpacity>
-                )}
+                           <TouchableOpacity onPress={() => handleBookmark(item)}>
+                                <Text style={styles.bookmark}>
+                                    {isBookmarked ? 'Remove Bookmark' : 'Bookmark'}
+                                </Text>
+                            </TouchableOpacity>
+                       </TouchableOpacity>
+                    );
+                }}
             />
 
             {selectedStory && (
@@ -71,12 +112,12 @@ const Authorized = ({ user, handleAuthentication }) => {
                     visible={modalVisible}
                     animationType='slide'
                     onRequestClose={() => setModalVisible(!modalVisible)}>
-                    <ScrollView contentContainerStyle={styles.modalContent}>
-                        <Text style={styles.modalTitle}>{selectedStory.title}</Text>
-                        <Text style={styles.modalAuthor}>By: {selectedStory.author}</Text>
-                        <Text style={styles.modalStory}>{selectedStory.story}</Text>
-                        <Text style={styles.modalMoral}>Moral Lesson: {selectedStory.moral}</Text>
-                        <Button color='#3A3967' title='Close' onPress={handleClearStory} />
+                    <ScrollView contentContainerStyle={[styles.modalContent, myBackground]}>
+                        <Text style={[styles.modalTitle, myColor2]}>{selectedStory.title}</Text>
+                        <Text style={[styles.modalAuthor, myColor2]}>By: {selectedStory.author}</Text>
+                        <Text style={[styles.modalStory, {color: isEnabled ? '#fff' : '#6A6A6A'}]}>{selectedStory.story}</Text>
+                        <Text style={[styles.modalMoral, myColor2]}>Moral Lesson: {selectedStory.moral}</Text>
+                        <Button color='red' title='Close' onPress={handleClearStory} />
                     </ScrollView>
                 </Modal>
             )}
@@ -88,7 +129,6 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: 50,
         padding: 16,
-        backgroundColor: '#FBF7EF',
         flex: 1,
     },
     loader: {
@@ -106,7 +146,6 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
     read: {
-        color: '#3A3967',
         fontWeight: 'bold',
         fontSize: 28,
     },
@@ -117,15 +156,14 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         fontSize: 16,
         fontWeight: '500',
-        color: '#3A3967',
     },
     logoutButton: {
         color: 'red',
         fontSize: 16,
         fontWeight: '500',
+        paddingBottom: 10,
     },
     storyItem: {
-        backgroundColor: '#fff',
         padding: 15,
         marginBottom: 10,
         borderRadius: 10,
@@ -147,37 +185,51 @@ const styles = StyleSheet.create({
         color: '#3A3967',
     },
     modalContent: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        // flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
         padding: 20,
-        backgroundColor: '#FBF7EF',
+        paddingTop: 70,
+        paddingBottom: 50,
+        height: 'auto',
     },
     modalTitle: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#3A3967',
         marginBottom: 20,
         textAlign: 'center',
+        textTransform: 'capitalize'
     },
     modalAuthor: {
         fontSize: 18,
-        color: '#3A3967',
         marginBottom: 20,
         textAlign: 'center',
+        textTransform: 'capitalize'
     },
     modalStory: {
         fontSize: 16,
-        color: '#6A6A6A',
         marginBottom: 20,
         textAlign: 'justify',
+        lineHeight: 40,
+        paddingTop: 20,
+        paddingBottom: 30,
     },
     modalMoral: {
         fontSize: 16,
         fontWeight: 'bold',
-        color: '#3A3967',
         marginBottom: 20,
         textAlign: 'center',
+        textTransform: 'capitalize',
+        lineHeight: 25,
+    },
+    readText: {
+        textDecorationLine: 'line-through',
+        color: 'gray',
+    },
+    bookmark: {
+        fontSize: 16,
+        color: 'blue',
+        marginTop: 8,
     },
 });
 
